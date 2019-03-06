6d85fefaedefd412787bcddd63e1023108466a75
Ross <ross.s.cockburn@gmail.com>
2019-03-06 22:42:53 +0000

    2d board array is created dynamically based on size of board entered by user

--- board.c
+++ board.c
@@ -5,14 +5,30 @@
 #define FALSE 0
 
 void setupBoard(int);
+void initialiseState(struct state **, int);
+
+struct state {
+    int player;
+    int **board;
+    
+};
 
 
 
 int main(void){
-    int size;
+    int size, i, j;
+    struct state *game_state;
     printf("enter size of board\n");
     scanf("%d", &size);
     setupBoard(size);
+    initialiseState(&game_state, size);
+
+    for (i =0; i<size; i++){
+        for (j=0; j<size; j++){
+            printf("%d ", game_state -> board[i][j]);
+        }
+    }
+    return 0;
 }
 
 //creates a visual board in the terminal based on the size input the user has given 
@@ -33,4 +49,21 @@ void setupBoard(int size){
             printf("\n");
         }
     }
+}
+
+void initialiseState(struct state **game_state, int boardSize){
+    int i, j;
+    *game_state = (struct state *) malloc (sizeof(game_state));
+    (*game_state) -> player = 1;
+    (*game_state) -> board = (int**) malloc(boardSize * sizeof(int *));
+    for (i=0; i<boardSize; i++){
+        (*game_state) -> board[i] = (int *) malloc(boardSize * sizeof(int *));
+    }
+    for (i =0; i<boardSize; i++){
+        for (j=0; j<boardSize; j++){
+            (*game_state) -> board[i][j] = 0;
+        }
+    }
+
+    
 }
