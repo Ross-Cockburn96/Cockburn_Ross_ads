8d38a6d15986c3d691bdf41c4889e31219638ef6
Ross <ross.s.cockburn@gmail.com>
2019-03-08 21:19:43 +0000

    beginning of game completion evaluation function

--- board.c
+++ board.c
@@ -7,101 +7,138 @@
 void printBoard(int, struct state *);
 void initialiseState(struct state **, int);
 void start(struct state **, int);
+int gameFinished(struct state *, int);
 
-struct state {
+struct state
+{
     int player;
     int **board;
-    
 };
-
 
 char p1[3][25] = {" o o o ", " o   o ", " o o o "};
 char p2[3][25] = {" x   x ", "   x   ", " x   x "};
 
-int main(void){
+int main(void)
+{
     int size, i, j;
     struct state *game_state;
-    
+
     printf("enter size of board\n");
     scanf("%d", &size);
     initialiseState(&game_state, size);
     printBoard(size, game_state);
-    
 
     start(&game_state, size);
     return 0;
 }
 
-void start(struct state **game_state, int size){
+void start(struct state **game_state, int size)
+{
     int finished = FALSE;
-    int x,y;
-    while (finished == FALSE){
-        printf("player %d, enter your move, grid is %d by %d", (*game_state) -> player, size, size);
+    int x, y;
+    while (finished == FALSE)
+    {
+        printf("player %d, enter your move, grid is %d by %d", (*game_state)->player, size, size);
         scanf("%d%d", &x, &y);
-        (*game_state) -> board[x-1][y-1] = (*game_state) -> player;
-        if ((*game_state) -> player == 1){
-            (*game_state) -> player = 2;
-        }else{
-            (*game_state) -> player = 1;
+        (*game_state)->board[y - 1][x - 1] = (*game_state)->player;
+        if ((*game_state)->player == 1)
+        {
+            (*game_state)->player = 2;
         }
-        printBoard(size, (*game_state) );
-        
+        else
+        {
+            (*game_state)->player = 1;
+        }
+        printBoard(size, (*game_state));
+        // int moveEffect = gameFinished((*game_state)->board, size);
+        // if (moveEffect == 1)
+        // {
+        //     printf("player %d wins", (*game_state)->player);
+        // }
+        // if (moveEffect == 2)
+        // {
+        //     printf("draw");
+        // }
     }
-    printf ("coords are x: %d and y %d", x,y);
+    printf("coords are x: %d and y %d", x, y);
 }
 
-//creates a visual board in the terminal based on the size input the user has given 
-void printBoard(int size, struct state *game_state){
-    int i, boardCount =0;
+int gameFinished(struct state *gameState, int size)
+{
+    int i, j;
+    for (i = 0; i < size; i++)
+    {
+        return;
+    }
+}
+//creates a visual board in the terminal based on the size input the user has given
+void printBoard(int size, struct state *game_state)
+{
+    printf("\n\n");
+    int i, boardCount = 0;
     char line[9] = "--------";
-    for (i=0; i<(size*4) - 1 ; i++){ //i represents each row of output
+    for (i = 0; i < (size * 4) - 1; i++)
+    { //i represents each row of output
         int j;
-        if (i % 4 == 0 && i != 0 ){
+        if (i % 4 == 0 && i != 0)
+        {
             boardCount++;
         }
-        if (i % 4 == 3 ){
-            for (j=0; j<size; j++){ //j represents a column of output (column is 8 characters wide)
+        if (i % 4 == 3)
+        {
+            printf("-");
+            for (j = 0; j < size; j++)
+            { //j represents a column of output (column is 8 characters wide)
                 printf("%s", line);
             }
             printf("\n");
-        }else{
-            for (j=0; j<size-1; j++){
-                if (game_state -> board[boardCount][j] == 0){
+        }
+        else
+        {
+            for (j = 0; j < size - 1; j++)
+            {
+                if (game_state->board[j][boardCount] == 0)
+                {
                     printf("\t|");
                 }
-                if (game_state -> board[boardCount][j] == 1){
-                    printf("%s|", p1[i % 4] );
+                if (game_state->board[j][boardCount] == 1)
+                {
+                    printf("%s|", p1[i % 4]);
                 }
-                if (game_state -> board[boardCount][j] == 2){
+                if (game_state->board[j][boardCount] == 2)
+                {
                     printf("%s|", p2[i % 4]);
                 }
             }
-            if(game_state -> board[boardCount][j] == 1){
-                printf("%s", p1[i %4]);
+            if (game_state->board[j][boardCount] == 1)
+            {
+                printf("%s", p1[i % 4]);
             }
-            if (game_state -> board[boardCount][j] == 2){
+            if (game_state->board[j][boardCount] == 2)
+            {
                 printf("%s", p2[i % 4]);
             }
             printf("\n");
         }
-        
     }
-    printf("\n\n----------------------------------------------------");
+    printf("\n\n----------------------------------------------------\n");
 }
 
-void initialiseState(struct state **game_state, int boardSize){
+void initialiseState(struct state **game_state, int boardSize)
+{
     int i, j;
-    *game_state = (struct state *) malloc (sizeof(game_state));
-    (*game_state) -> player = 1;
-    (*game_state) -> board = (int**) malloc(boardSize * sizeof(int *));
-    for (i=0; i<boardSize; i++){
-        (*game_state) -> board[i] = (int *) malloc(boardSize * sizeof(int *));
+    *game_state = (struct state *)malloc(sizeof(game_state));
+    (*game_state)->player = 1;
+    (*game_state)->board = (int **)malloc(boardSize * sizeof(int *));
+    for (i = 0; i < boardSize; i++)
+    {
+        (*game_state)->board[i] = (int *)malloc(boardSize * sizeof(int *));
     }
-    for (i =0; i<boardSize; i++){
-        for (j=0; j<boardSize; j++){
-            (*game_state) -> board[i][j] = 0;
+    for (i = 0; i < boardSize; i++)
+    {
+        for (j = 0; j < boardSize; j++)
+        {
+            (*game_state)->board[i][j] = 0;
         }
     }
-
-    
 }
